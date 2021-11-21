3b26b94474f52260716531a50c0e09c18c51bebb
jm11116 <92898363+jkl112@users.noreply.github.com>
2021-11-21 15:34:50 +1100

    console.logging of elements added.
    Still needs backward compatibility testing and a favicon.

--- js/animations.js
+++ js/animations.js
@@ -6,8 +6,11 @@ class Animations {
         this.hideElements();
         this.startAnimation();
         this.bindRollovers();
+        this.block2offset = document.getElementById("main_heading2").offsetTop;
+        this.trackScroll();
+        this.consoleLogClickedElems();
     }
-    hideElements(){
+    hideElements(){ //Elements are hidden with JS so that users with JS disabled can still use program
         $(this.gallery_elems).animate({"opacity": "0"}, 0).css("margin-left", "-10px");
         $(this.heading_elems).animate({"opacity": "0"}, 0).css("margin-left", "-10px");
         $(this.article_elems).animate({"opacity": "0"}, 0).css("margin-left", "-10px");
@@ -19,22 +22,30 @@ class Animations {
         });
     }
     animateDocument() {
-        var timeout = 1000;
+        var timeout = 760;
         $(this.gallery_elems).each(function(){
-            $(this).animate({"opacity": "1", "margin-left": "0px"}, timeout);
-            timeout = timeout + 300;
+            setTimeout(() => {
+                $(this).animate({"opacity": "1", "margin-left": "0px"}, 800);
+            }, timeout);
+            timeout = timeout + 100;
         });
         setTimeout(() => {
             $(this.heading_elems).each(function(){
-                $(this).animate({"opacity": "1", "margin-left": "0px"}, 1000);
-            });
-            $(this.article_elems).each(function(){
                 $(this).animate({"opacity": "1", "margin-left": "0px"}, 1000);
             });
             $("p").each(function(){
                 $(this).animate({"opacity": "1", "margin-left": "0px"}, 1000);
             });
         }, 500);
+    }
+    fadeInCards(){
+        var timeout = 100;
+        $(this.article_elems).each(function(){
+            setTimeout(() => {
+                $(this).animate({"opacity": "1", "margin-left": "0px"}, 800);
+            }, timeout);
+            timeout = timeout + 100;
+        });
     }
     bindRollovers(){
         $(".gallery_img, .card_img").mouseover(function(){
@@ -48,6 +59,20 @@ class Animations {
         });
         $("a").mouseout(function(){
             $(this).animate({"margin-left": "0px"}, 200);
+        });
+    }
+    trackScroll(){
+        $(window).scroll((e) => {
+            var current_scroll_pos = document.documentElement.scrollTop || document.body.scrollTop;
+            if (current_scroll_pos > this.block2offset - 400){
+                this.fadeInCards();
+            }
+        });
+    }
+    consoleLogClickedElems(){
+        var clickable_elems = ".gallery_img, .card_img, .read_more";
+        $(clickable_elems).click(function(){
+            console.log($(this).attr("data-descript"));
         });
     }
 }
